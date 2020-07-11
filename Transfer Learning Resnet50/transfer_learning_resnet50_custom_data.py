3d449bea812c921842dd00f89a75c0a515a85915
Sobhan Mondal <sobhaniiest@gmail.com>
2020-07-11 16:54:45 +0530

    Update transfer_learning_resnet50_custom_data.py

--- Transfer Learning Resnet50/transfer_learning_resnet50_custom_data.py
+++ Transfer Learning Resnet50/transfer_learning_resnet50_custom_data.py
@@ -3,7 +3,7 @@ import os
 import time
 from resnet50 import ResNet50
 from keras.preprocessing import image
-from keras.layers import GlobalAveragePooling2D, Dense, Dropout,Activation,Flatten
+from keras.layers import GlobalAveragePooling2D, GlobalMaxPooling2D, Dense, Dropout,Activation,Flatten
 
 from imagenet_utils import preprocess_input
 from keras.layers import Input
@@ -11,18 +11,21 @@ from keras.models import Model
 from keras.utils import np_utils
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+
+#############################################################################################################################
 
 Img_size = 512
-epochs = 20
-batch_size = 32
+epochs = 25
+batch_size = 64
 
 # Loading the training data
-PATH = os.getcwd()
 # Define data path
-data_path = PATH + '/data'
+data_path = '/content/drive/My Drive/FBH'
 data_dir_list = os.listdir(data_path)
 
 img_data_list=[]
+
 
 for dataset in data_dir_list:
 	img_list=os.listdir(data_path+'/'+ dataset)
@@ -46,16 +49,17 @@ print (img_data.shape)
 
 
 # Define the number of classes
-num_classes = 2
+num_classes = 5
 num_of_samples = img_data.shape[0]
 labels = np.ones((num_of_samples,),dtype='int64')
 
-labels[0:200]=0
-labels[200:]=1
-#labels[404:606]=2
-#labels[606:]=3
+labels[0:150]=0
+labels[150:300]=1
+labels[300:450]=2
+labels[450:600]=3
+labels[600:]=4
 
-#names = ['01', '02']
+
 # convert class labels to on-hot encoding
 Y = np_utils.to_categorical(labels, num_classes)
 
@@ -65,6 +69,28 @@ x,y = shuffle(img_data,Y, random_state=2)
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
 
 ###########################################################################################################################
+
+from keras import backend as K
+
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def f1_m(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
+
+#############################################################################################################################
+
 # Custom_resnet_model_1
 #Training the classifier alone
 image_input = Input(shape=(Img_size, Img_size, 3))
@@ -82,30 +108,47 @@ for layer in custom_resnet_model.layers[:-1]:
 
 custom_resnet_model.layers[-1].trainable
 
-custom_resnet_model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+custom_resnet_model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy', f1_m, precision_m, recall_m])
 
 t=time.time()
 hist = custom_resnet_model.fit(X_train, y_train, batch_size, epochs, verbose=1, validation_data=(X_test, y_test))
 print('Training time: %s' % (t - time.time()))
-(loss, accuracy) = custom_resnet_model.evaluate(X_test, y_test, batch_size=10, verbose=1)
+(loss, accuracy, f1_score, precision, recall) = custom_resnet_model.evaluate(X_test, y_test, batch_size=10, verbose=1)
 
-print("[INFO] loss={:.4f}, accuracy: {:.4f}%".format(loss,accuracy * 100))
+print("[INFO] loss={:.4f}, accuracy: {:.4f}%, F1-score={:.4f}, Precision={:.4f}, Recall={:.4f}".format(loss, accuracy * 100, f1_score, precision, recall))
+
+###########################################################################################################################
+
+from sklearn.metrics import classification_report, confusion_matrix
+y_pred = custom_resnet_model.predict(X_test)
+print("Class")
+print(y_test)
+matrix = confusion_matrix(y_test.argmax(axis=1), y_pred.argmax(axis=1))
+print('Confusion Matrix')
+print(matrix)
 
 ###########################################################################################################################
 
 # Fine tune the resnet 50
-#image_input = Input(shape=(224, 224, 3))
-model = ResNet50(weights='imagenet',require_flatten=False)
+
+image_input = Input(shape=(Img_size, Img_size, 3))
+model = ResNet50(input_tensor=image_input, weights='imagenet', require_flatten=False)
 model.summary()
 last_layer = model.output
 # add a global spatial average pooling layer
 x = GlobalAveragePooling2D()(last_layer)
+
 # add fully-connected & dropout layers
 x = Dense(512, activation='relu',name='fc-1')(x)
 x = Dropout(0.5)(x)
 x = Dense(256, activation='relu',name='fc-2')(x)
 x = Dropout(0.5)(x)
-# a softmax layer for 4 classes
+
+###
+#x = Dense(128, activation='relu',name='fc-3')(x)
+#x = Dropout(0.5)(x)
+###
+# a softmax layer for 5 classes
 out = Dense(num_classes, activation='softmax',name='output_layer')(x)
 
 # this is the model we will train
@@ -118,16 +161,17 @@ for layer in custom_resnet_model2.layers[:-6]:
 
 custom_resnet_model2.layers[-1].trainable
 
-custom_resnet_model2.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+custom_resnet_model2.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy', f1_m, precision_m, recall_m])
 
 t=time.time()
 hist = custom_resnet_model2.fit(X_train, y_train, batch_size, epochs, verbose=1, validation_data=(X_test, y_test))
 print('Training time: %s' % (t - time.time()))
-(loss, accuracy) = custom_resnet_model2.evaluate(X_test, y_test, batch_size=10, verbose=1)
+(loss, accuracy, f1_score, precision, recall) = custom_resnet_model2.evaluate(X_test, y_test, batch_size=10, verbose=1)
 
-print("[INFO] loss={:.4f}, accuracy: {:.4f}%".format(loss,accuracy * 100))
+print("[INFO] loss={:.4f}, accuracy: {:.4f}%, F1-score={:.4f}, Precision={:.4f}, Recall={:.4f}".format(loss, accuracy * 100, f1_score, precision, recall))
 
-############################################################################################
+#################################################################################################################################
+
 import matplotlib.pyplot as plt
 # visualizing losses and accuracy
 train_loss=hist.history['loss']
@@ -157,3 +201,5 @@ plt.grid(True)
 plt.legend(['train','val'],loc=4)
 #print plt.style.available # use bmh, classic,ggplot for big pictures
 plt.style.use(['classic'])
+
+#####################################################################################################################################
